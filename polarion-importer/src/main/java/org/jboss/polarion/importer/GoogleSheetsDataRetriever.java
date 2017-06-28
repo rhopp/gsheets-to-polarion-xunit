eb7cd9aac3a0b03f485e21e4f86bd024ff7e9871
rhopp <rhopp@redhat.com>
2017-06-28 16:15:59 +0200

    Parameters for specifying columns for ID, verdict and comment added.

--- polarion-importer/src/main/java/org/jboss/polarion/importer/GoogleSheetsDataRetriever.java
+++ polarion-importer/src/main/java/org/jboss/polarion/importer/GoogleSheetsDataRetriever.java
@@ -24,110 +24,116 @@ import com.google.api.services.sheets.v4.model.ValueRange;
 public class GoogleSheetsDataRetriever {
 
 	/** Application name. */
-    private static final String APPLICATION_NAME =
-        "Google Sheets API Java Quickstart";
+	private static final String APPLICATION_NAME = "Google Sheets API Java Quickstart";
 
-    /** Directory to store user credentials for this application. */
-    private static final java.io.File DATA_STORE_DIR = new java.io.File(
-        System.getProperty("user.home"), ".credentials/sheets.googleapis.com-java-quickstart");
+	/** Directory to store user credentials for this application. */
+	private static final java.io.File DATA_STORE_DIR = new java.io.File(System.getProperty("user.home"),
+			".credentials/sheets.googleapis.com-java-quickstart");
 
-    /** Global instance of the {@link FileDataStoreFactory}. */
-    private static FileDataStoreFactory DATA_STORE_FACTORY;
+	/** Global instance of the {@link FileDataStoreFactory}. */
+	private static FileDataStoreFactory DATA_STORE_FACTORY;
 
-    /** Global instance of the JSON factory. */
-    private static final JsonFactory JSON_FACTORY =
-        JacksonFactory.getDefaultInstance();
+	/** Global instance of the JSON factory. */
+	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
-    /** Global instance of the HTTP transport. */
-    private static HttpTransport HTTP_TRANSPORT;
+	/** Global instance of the HTTP transport. */
+	private static HttpTransport HTTP_TRANSPORT;
 
-    /** Global instance of the scopes required by this quickstart.
-     *
-     * If modifying these scopes, delete your previously saved credentials
-     * at ~/.credentials/sheets.googleapis.com-java-quickstart
-     */
-    private static final List<String> SCOPES =
-        Arrays.asList(SheetsScopes.SPREADSHEETS_READONLY);
+	/**
+	 * Global instance of the scopes required by this quickstart.
+	 *
+	 * If modifying these scopes, delete your previously saved credentials at
+	 * ~/.credentials/sheets.googleapis.com-java-quickstart
+	 */
+	private static final List<String> SCOPES = Arrays.asList(SheetsScopes.SPREADSHEETS_READONLY);
 
-    static {
-        try {
-            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
-        } catch (Throwable t) {
-            t.printStackTrace();
-            System.exit(1);
-        }
-    }
+	static {
+		try {
+			HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+			DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+		} catch (Throwable t) {
+			t.printStackTrace();
+			System.exit(1);
+		}
+	}
 
-    /**
-     * Creates an authorized Credential object.
-     * @return an authorized Credential object.
-     * @throws IOException
-     */
-    public static Credential authorize() throws IOException {
-        // Load client secrets.
-        InputStream in =
-            PolarionImporter.class.getResourceAsStream("/client_secret.json");
-        GoogleClientSecrets clientSecrets =
-            GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+	/**
+	 * Creates an authorized Credential object.
+	 * 
+	 * @return an authorized Credential object.
+	 * @throws IOException
+	 */
+	public static Credential authorize() throws IOException {
+		// Load client secrets.
+		InputStream in = PolarionImporter.class.getResourceAsStream("/client_secret.json");
+		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow =
-                new GoogleAuthorizationCodeFlow.Builder(
-                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(DATA_STORE_FACTORY)
-                .setAccessType("offline")
-                .build();
-        Credential credential = new AuthorizationCodeInstalledApp(
-            flow, new LocalServerReceiver()).authorize("user");
-        System.out.println(
-                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
-        return credential;
-    }
+		// Build flow and trigger user authorization request.
+		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
+				clientSecrets, SCOPES).setDataStoreFactory(DATA_STORE_FACTORY).setAccessType("offline").build();
+		Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+		System.out.println("Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+		return credential;
+	}
 
-    /**
-     * Build and return an authorized Sheets API client service.
-     * @return an authorized Sheets API client service
-     * @throws IOException
-     */
-    public static Sheets getSheetsService() throws IOException {
-        Credential credential = authorize();
-        return new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-    }
-	
-    
-	public static List<TestCaseVerdict> getVerdicts(String spreadsheetId) throws IOException {
-	     // Build a new authorized API client service.
+	/**
+	 * Build and return an authorized Sheets API client service.
+	 * 
+	 * @return an authorized Sheets API client service
+	 * @throws IOException
+	 */
+	public static Sheets getSheetsService() throws IOException {
+		Credential credential = authorize();
+		return new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName(APPLICATION_NAME)
+				.build();
+	}
+
+	public static List<TestCaseVerdict> getVerdicts(String spreadsheetId, int idColumn, int verdictColumn,
+			int commentColumn) throws IOException {
+		// Build a new authorized API client service.
 		Sheets service = getSheetsService();
-       
-       List<TestCaseVerdict> verdicts = new ArrayList<TestCaseVerdict>();
 
-       // Prints the names and majors of students in a sample spreadsheet:
-       // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-//       String spreadsheetId = "1ChqrLRZR8FZYwXR6Ehj3u5WwKzigBxDz49v1sn5TNfE";
-       String range = "Basic Report!A2:L";
-       ValueRange response = service.spreadsheets().values()
-           .get(spreadsheetId, range)
-           .execute();
-       List<List<Object>> values = response.getValues();
-       if (values == null || values.size() == 0) {
-           System.out.println("No data found.");
-       } else {
-         for (List row : values) {
-           if (row.size()<10) {
-           	System.out.println("Skipping test case ID:"+row.get(0));
-           }else {
-           	TestCaseVerdict testCaseVerdict = new TestCaseVerdict((String) row.get(0), TestCaseVerdictEnum.lookup((String) row.get(10)));
-           	if (row.size()>11) {
-           		testCaseVerdict.setComment((String) row.get(11));
-           	}
-           	verdicts.add(testCaseVerdict);
-           }
-         }
-       }
-       return verdicts;
-//       verdicts.stream().forEach(verdict -> System.out.println(verdict.toString()));
+		List<TestCaseVerdict> verdicts = new ArrayList<TestCaseVerdict>();
+
+		// Prints the names and majors of students in a sample spreadsheet:
+		// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+		// String spreadsheetId = "1ChqrLRZR8FZYwXR6Ehj3u5WwKzigBxDz49v1sn5TNfE";
+		String range = "Basic Report!A2:M";
+		ValueRange response = service.spreadsheets().values().get(spreadsheetId, range).execute();
+		List<List<Object>> values = response.getValues();
+		processData(verdicts, values, idColumn, verdictColumn, commentColumn);
+		return verdicts;
+	}
+
+	private static void processData(List<TestCaseVerdict> verdicts, List<List<Object>> values, int idColumn,
+			int verdictColumn, int commentColumn) {
+		if (values == null || values.size() == 0) {
+			System.out.println("No data found.");
+		} else {
+			for (List<Object> row : values) {
+				if (isInBounds(row, idColumn)) {
+					if (isInBounds(row, verdictColumn)) {
+						TestCaseVerdict testCaseVerdict = new TestCaseVerdict((String) row.get(idColumn),
+								TestCaseVerdictEnum.lookup((String) row.get(verdictColumn)));
+						if (isInBounds(row, commentColumn)) {
+							testCaseVerdict.setComment((String) row.get(commentColumn));
+						} else {
+							System.out.println(
+									String.format("Unable to find comment for test case ID: %s", row.get(idColumn)));
+						}
+						verdicts.add(testCaseVerdict);
+					} else {
+						System.out.println(String.format("Unable to find verdict for test case ID %s. Skipping.",
+								row.get(idColumn)));
+					}
+				} else {
+					throw new RuntimeException(String.format("Column %s is out of bounds", idColumn));
+				}
+			}
+		}
+	}
+
+	private static boolean isInBounds(List<Object> array, int position) {
+		return (position >= 0) && (position < array.size());
 	}
 }
--- polarion-importer/src/main/java/org/jboss/polarion/importer/PolarionImporter.java
+++ polarion-importer/src/main/java/org/jboss/polarion/importer/PolarionImporter.java
@@ -4,27 +4,36 @@ import java.io.IOException;
 import java.util.List;
 
 public class PolarionImporter {
-	
+
 	private static String PROJECT_ID = "JBDS";
-	
+
 	private static String TEST_RUN_ID;
 	private static final String TEST_RUN_ID_KEY = "testrun.id";
-	
+
 	private static String SHEET_ID;
 	private static final String SHEET_ID_KEY = "sheet.id";
-	
-    
 
-    public static void main(String[] args) throws IOException {
-    	populateParameters();
-    	
-        List<TestCaseVerdict> verdicts = GoogleSheetsDataRetriever.getVerdicts(SHEET_ID);
-        
-        System.out.println(XUnitXMLGenerator.generateXML(verdicts, PROJECT_ID, TEST_RUN_ID));
-        
-    }
+	private static Character ID_COLUMN_CHAR;
+	private static final String ID_COLUMN_CHAR_KEY = "id.column.char";
 
-    private static void populateParameters() {
+	private static Character VERDICT_COLUMN_CHAR;
+	private static final String VERDICT_COLUMN_CHAR_KEY = "verdict.column.char";
+
+	private static Character COMMENT_COLUMN_CHAR;
+	private static final String COMMENT_COLUMN_CHAR_KEY = "comment.column.char";
+
+	public static void main(String[] args) throws IOException {
+		populateParameters();
+
+		List<TestCaseVerdict> verdicts = GoogleSheetsDataRetriever.getVerdicts(SHEET_ID,
+				charToIntAlphabetically(ID_COLUMN_CHAR), charToIntAlphabetically(VERDICT_COLUMN_CHAR),
+				charToIntAlphabetically(COMMENT_COLUMN_CHAR));
+
+		System.out.println(XUnitXMLGenerator.generateXML(verdicts, PROJECT_ID, TEST_RUN_ID));
+
+	}
+
+	private static void populateParameters() {
 		SHEET_ID = System.getProperty(SHEET_ID_KEY);
 		if (SHEET_ID == null) {
 			throw new RuntimeException(String.format("System property %s must be set", SHEET_ID_KEY));
@@ -33,6 +42,34 @@ public class PolarionImporter {
 		if (TEST_RUN_ID == null) {
 			throw new RuntimeException(String.format("System property %s must be set", TEST_RUN_ID_KEY));
 		}
+		ID_COLUMN_CHAR = parameterShouldBeChar(System.getProperty(ID_COLUMN_CHAR_KEY));
+		if (ID_COLUMN_CHAR == null) {
+			throw new RuntimeException(String.format("System property %s must be set", ID_COLUMN_CHAR_KEY));
+		}
+		VERDICT_COLUMN_CHAR = parameterShouldBeChar(System.getProperty(VERDICT_COLUMN_CHAR_KEY));
+		if (VERDICT_COLUMN_CHAR == null) {
+			throw new RuntimeException(String.format("System property %s must be set", VERDICT_COLUMN_CHAR_KEY));
+		}
+		COMMENT_COLUMN_CHAR = parameterShouldBeChar(System.getProperty(COMMENT_COLUMN_CHAR_KEY));
+		if (COMMENT_COLUMN_CHAR == null) {
+			throw new RuntimeException(String.format("System property %s must be set", ID_COLUMN_CHAR_KEY));
+		}
+	}
+
+	private static Character parameterShouldBeChar(String value) {
+		if (value.length() != 1) {
+			return null;
+		}
+		Character c = value.charAt(0);
+		if (!Character.isLetter(c)) {
+			return null;
+		}
+		return c;
+	}
+
+	private static int charToIntAlphabetically(Character c) {
+		c = Character.toLowerCase(c);
+		return c.charValue() - 97; // a=0
 	}
 
 }
